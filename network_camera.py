75fc1963cc222c41779bc071bbc9a4fcd6e3fc29
Hannes Mogensen <hmogensen@users.noreply.github.com>
2024-05-03 19:54:08 +0200

    Add time-lapse routine for network cameras

---
+++ network_camera.py
@@ -0,0 +1,104 @@
+import cv2
+import time
+import logging
+import os
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+class NetworkCamera:
+
+    def __init__(self, *, url:str, location:str, flush_frames:int, image_interval_s:float):
+        self.url = url
+        self.location = location
+        self.is_connected = False
+        self.cap = None
+        self.flush_frames = flush_frames
+        self.image_interval_s = image_interval_s
+        os.makedirs(self.location, exist_ok=True)
+
+    def start_capture(self, persistent=True):
+        if persistent:
+            self.persistent_capture()
+        else:
+            self.intermittent_capture()
+
+    def persistent_capture(self):
+        self.open_capture()
+        while True:
+            try:
+                success = self.capture_image()
+                if success:
+                    time.sleep(self.image_interval_s)
+                else:
+                    self.restart()
+            except Exception as exc:
+                logger.exception("Exception during persistent capture")
+                self.restart()
+
+    def intermittent_capture(self):
+        while True:
+            try:
+                self.capture_single_image()
+                time.sleep(self.image_interval_s)
+            except Exception as exc:
+                logger.exception("Exception during intermittent capture")
+                time.sleep(10)
+
+    def capture_image(self):
+        if self.flush_frames:
+            time.sleep(1)
+            for i in range(self.flush_frames):
+                success = self.cap.grab()
+                if not success:
+                    logger.error(f"Flushing error {i} / {self.flush_frames}")
+                    return False
+            time.sleep(1)
+
+        success, frame = self.cap.read()
+        if success:
+            timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+            filename = os.path.join(self.location, f"{self.location}-{timestamp}.png")
+            cv2.imwrite(filename, frame)
+            logger.info(f"Saved image: {filename}")
+        else:
+            logger.error("Image capturing error")
+        return success
+    
+    def open_capture(self):
+        self.cap = cv2.VideoCapture(self.url)
+        if not self.cap.isOpened():
+            logger.error("Error opening rtsp stream")
+            self.is_connected = False
+        else:
+            logger.debug("Opening capture successful")
+            self.is_connected = True
+
+    def release_capture(self):
+        if self.cap is not None:
+            try:
+                self.cap.release()
+                logger.debug("Releasing capture successful")
+            except Exception as exc:
+                logger.exception("Exception thrown releasing capture")
+        self.cap = None
+        self.is_connected = False
+
+    def restart(self):
+        self.release_capture()
+        time.sleep(10)
+        self.open_capture()
+
+    def capture_single_image(self):
+        # May put more strain on camera hardware if used excessively
+        self.open_capture()
+        self.capture_image()
+        self.release_capture()
+        
---
+++ timelapse.py
@@ -0,0 +1,48 @@
+from network_camera import NetworkCamera
+import configparser
+import argparse
+
+default_flush_frames = 100
+default_image_interval_s = 600
+
+def record_timelapse(cam_id:str, location:str, flush_frames:int, image_interval_s:float):
+    
+    config = configparser.ConfigParser()
+    config.read('credentials.txt')
+    
+    if cam_id not in config:
+        raise ValueError(f"Camera type '{cam_id}' not found in credentials.txt")
+    
+    cam_ip = config[cam_id]['ip']
+    username = config[cam_id]['username']
+    password = config[cam_id]['password']
+    stream_path = config[cam_id]['stream_path']
+    
+    if "axis" in cam_id:
+        rtsp_url = f"rtsp://{username}:{password}@{cam_ip}/{stream_path}"
+    else:
+        rtsp_url = f"rtsp://{username}:{password}@{cam_ip}:554/{stream_path}"
+    
+    cam = NetworkCamera(url=rtsp_url, location=location, flush_frames=flush_frames, image_interval_s=image_interval_s)
+    cam.start_capture(persistent=True)
+    
+    return cam
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Time lapse recording with network cameras')
+    parser.add_argument('cam', help='Camera id')
+    parser.add_argument('loc', type=str, help='Location (e.g. "garden") or portrayed object (e.g. "sprouting plant")')
+    parser.add_argument('--flush', '-f', type=int, default=default_flush_frames, 
+                        help=f'Number of frames to flush. Default: {default_flush_frames}')
+    parser.add_argument('--interval', '-i', type=int, default=default_image_interval_s, 
+                        help=f'Interval between image captures in seconds. Default: {default_image_interval_s}')
+    
+    args = parser.parse_args()
+    
+    record_timelapse(
+        cam_id=args.cam,
+        location=args.loc,
+        flush_frames=args.flush,
+        image_interval_s=args.interval
+    )
